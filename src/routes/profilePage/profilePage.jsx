34952f139852c7ac6a40036b8738fd50feacb1ce
Mike Rembo <rembomike@gmail.com>
2025-12-07 13:01:54 +0300

    update profile success

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -2,15 +2,19 @@ import Chat from '../../components/chat/Chat';
 import List from '../../components/list/List';
 import './profilePage.scss';
 import apiRequest from './../../lib/apiRequest';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 function ProfilePage() {
+  const { currentUser, updateUser } = useContext(AuthContext);
+
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      const res = await apiRequest.post('/auth/logout');
-      localStorage.removeItem('user');
+      await apiRequest.post('/auth/logout');
+      updateUser(null);
       navigate('/');
     } catch (err) {
       console.log(err);
@@ -22,21 +26,20 @@ function ProfilePage() {
         <div className="wrapper">
           <div className="title">
             <h1>User Information</h1>
-            <button>Update Profile</button>
+            <Link to="/profile/update">
+              <button>Update Profile</button>
+            </Link>
           </div>
           <div className="info">
             <span>
               Avatar:
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
+              <img src={currentUser.avatar || '/noavatar.png'} alt="" />
             </span>
             <span>
-              Username: <b>John Doe</b>
+              Username: <b>{currentUser.username}</b>
             </span>
             <span>
-              E-mail: <b>john@gmail.com</b>
+              E-mail: <b>{currentUser.email}</b>
             </span>
             <button onClick={handleLogout}>Logout</button>
           </div>
